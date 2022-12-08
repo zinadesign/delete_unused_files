a18193063d6bf979552f1229e83b33a4b4124c05
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 23:08:00 +0000

    Adding tarfile member sanitization to extractall()

--- restore_deleted_unused_files.py
+++ restore_deleted_unused_files.py
@@ -15,7 +15,26 @@ def get_answer(default='Y'):
 def restore_deleted_unused_files(restore_files_from_archive):
     with tarfile.open(restore_files_from_archive, mode='r:') as archive:
         print('Extracting files...')
-        archive.extractall('/')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, "/")
         print('All files extracted.')
 
 if __name__ == '__main__':
